3cbc97597aa7554ec9ff9532f96696af343fe96b
Dnyanu90 <131323037+Dnyanu90@users.noreply.github.com>
2026-06-27 13:28:55 +0530

    Add Welcome Message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World! Tecspeak");
+        System.out.println("Welcome to the Repo");
     }
 }
